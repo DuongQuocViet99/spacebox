a7f7b4ed7f3c97ca298641ad6aecbd712291ba80
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-12-12 23:45:17 +0700

    new colors, custom & use new switch

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -1,15 +1,28 @@
 import { extendTheme } from '@chakra-ui/react'
+import { switchTheme } from './switch'
 
 export const theme = extendTheme({
   colors: {
     adobe: {
       gray: {
         1: '#88898C',
-        2: '#6E6F73'
+        2: '#6E6F73',
+        3: '#D9D9D9',
+        4: '#E6E6E6'
       },
       cyan: {
         1: '#3DD1F2'
+      },
+      brown: {
+        1: '#BFB9B8',
+        2: '#A6A1A1'
+      },
+      pink: {
+        1: '#FAC8CE'
       }
     }
+  },
+  components: {
+    Switch: switchTheme
   }
 })
