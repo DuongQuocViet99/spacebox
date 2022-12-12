c0f8a2eb1e1ca57882888bc1b197121e4ea5a51e
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-12-12 18:58:38 +0700

    create: custom theme

---
+++ src/components/theme.tsx
@@ -0,0 +1,15 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    adobe: {
+      gray: {
+        1: '#88898C',
+        2: '#6E6F73'
+      },
+      cyan: {
+        1: '#3DD1F2'
+      }
+    }
+  }
+})
