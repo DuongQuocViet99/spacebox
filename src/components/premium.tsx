ff3a9df8ff22df1ad76c463964c37a620997b413
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-12-13 10:37:10 +0700

    new: Premium

---
+++ src/components/premium.tsx
@@ -0,0 +1,15 @@
+import * as TB from '@tabler/icons'
+import * as C from '@chakra-ui/react'
+
+export default function Premium() {
+  return (
+    <C.HStack
+      p={ 2 }
+      rounded='md'
+      boxShadow='base'
+    >
+      <TB.IconDiamonds color='#FAC8CE' />
+      <C.Switch size='sm' />
+    </C.HStack>
+  )
+}
