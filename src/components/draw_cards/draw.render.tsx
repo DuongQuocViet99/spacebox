b8b49f661788b3eec4f64c388047f4056071fdc9
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-12-21 22:58:07 +0700

    render all category of card

---
+++ src/components/draw_cards/draw.render.tsx
@@ -0,0 +1,19 @@
+interface ICategory {
+  name: string,
+  class: string,
+  percent: number
+}
+
+const listCategory: ICategory[] = [
+  { name: 'Vô cực', class: 'infinity', percent: 0.1 },
+  { name: 'Tối thượng', class: 'supreme', percent: 1 },
+  { name: 'Huyền thoại', class: 'legendary', percent: 3 },
+  { name: 'God', class: 'god', percent: 12 },
+  { name: 'Sử thi', class: 'epic', percent: 34 },
+  { name: 'Hiếm', class: 'rare', percent: 43 },
+  { name: 'Thường', class: 'normal', percent: 72 },
+]
+
+export default function DrawCardsRender({ render }: { render: ( i: ICategory, k: number ) => JSX.Element }) {
+  return <>{ listCategory.map(( i, k ) => render( i, k )) }</>
+}
