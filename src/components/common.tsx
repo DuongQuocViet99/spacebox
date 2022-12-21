95a361902fd9ca4bfdacce96f80dbceffd43925f
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-12-22 02:33:11 +0700

    common file for export

---
+++ src/components/common.tsx
@@ -0,0 +1,22 @@
+// Pages
+export { default as HomePage } from 'pages/home'
+export { default as ErrorPage } from 'pages/error'
+// Components
+export { theme } from 'components/theme'
+export { default as Money } from 'components/money'
+export { default as Layout } from 'components/layout'
+export { default as Header } from 'components/header'
+export { IconButtonRoute } from 'components/iconbutton'
+export { default as Premium } from 'components/premium'
+// - Drawer routes
+export { default as DrawerRoutes } from 'components/drawer_routes/drawer'
+export { default as DrawerRoutesRender } from 'components/drawer_routes/routes.render'
+//  - Drawer auth
+export { default as Card } from 'components/drawer_auth/card'
+export { default as DrawerAuth } from 'components/drawer_auth/drawer'
+// - Draw cards
+export { default as GetCard } from 'components/draw_cards/get'
+export { default as DrawCard } from 'components/draw_cards/draw'
+export { default as InfoCard } from 'components/draw_cards/info'
+export { default as InfoCardRender } from 'components/draw_cards/info.render'
+export { default as CategoryRender } from 'components/draw_cards/category.render'
